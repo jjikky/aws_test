9f31683c63b9fd51c294e31ed6be119ca3b11e86
jikky5418 <kok5418@naver.com>
2021-01-29 20:53:51 +0900

    1,2,3 : express, git, babel, nodemon, middleware

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-var express = require('express');
-var app = express();
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
 
 const PORT = 5003;
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
 app.get('/', function (req, res) {
     res.send('Hello world');
 });
