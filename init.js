dcab7b4685561dc05191c904bf48994e7bc65088
jikky5418 <kok5418@naver.com>
2021-01-30 15:58:42 +0900

    create order_model(v.0,1)

--- init.js
+++ init.js
@@ -1,6 +1,9 @@
+import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 5003;
+const PORT = process.env.PORT || 5003;
 
 const handleListening = () =>
     console.log(`✅ Listening on: http://localhost:${PORT}`);
---
+++ models/order.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema({
+    fileUrl: {
+        type: String
+    },
+    title: {
+        type: String,
+        required: "Tilte is required"
+    },
+    description: {
+        type: String,
+        required: "Description is required"
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    category: {
+        type: string,
+        required: "Category is required"
+    },
+    location: [
+        {
+            latitude: Number,
+            longitude: Number
+        }
+    ],
+    time: [
+        {
+            date: String,
+            hour: String
+        }
+    ],
+    status: {
+        type: String,
+        default: "진행중"
+    },
+    reward: {
+        type: Number,
+        required: "Description is required"
+    }
+});
+
+const model = mongoose.model("Order", OrderSchema);
+export default model;
